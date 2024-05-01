1f93e759996c55efef6cc4e3301a622b6b68a851
Anna Poglitsch <anna.poglitsch@outlook.de>
2024-05-01 18:06:39 +0200

    Created Classes User, Player and Bot.

---
+++ Medieval-Clash.Lib/User/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medieval_Clash.Lib.User
+{
+    internal class Player : User
+    {
+        private string ProfilePictureFileName { get; set; }
+        public Player(string name, int rating, int healthPoints, int manaPoints, int money, string profilePictureFileName) : base(name, rating, healthPoints, manaPoints, money)
+        {
+            ProfilePictureFileName = profilePictureFileName;
+        }
+    }
+}
